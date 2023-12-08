07e43e9721c66723067fc21195e668e6e3bd26dc
Alec Lima <aleclimadev@gmail.com>
2023-12-07 23:35:11 -0300

    feat: component Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import loading from "../../assets/images/loading.gif"
+import { Container } from "./style"
+
+
+const Loading = () => {
+  return (
+    <Container>
+        <img src={loading} alt="loading..." />
+    </Container>
+  )
+}
+
+export default Loading
