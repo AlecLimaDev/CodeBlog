a08d23d81c9287466591846ef0680941ee74f708
Alec Lima <aleclimadev@gmail.com>
2023-12-13 16:32:49 -0300

    protect key

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
-  projectId: "codeblog-d6e57",
+  projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
