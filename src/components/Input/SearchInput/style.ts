ce0e70cfb8e2a8b5190278fd323f8f7cb4519a61
Alec Lima <aleclimadev@gmail.com>
2023-12-19 13:35:22 -0300

    chore

---
+++ src/components/Input/SearchInput/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SearchInput = styled.input`
+    width: 40%;
+    margin-bottom: 40px;
+`
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
